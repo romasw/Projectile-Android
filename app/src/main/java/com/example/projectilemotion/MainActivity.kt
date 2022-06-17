3491a3501d686a80f03a1471fb0565fcaf8f0796
st18180kn <70875769+st18180kn@users.noreply.github.com>
2022-06-17 15:05:08 +0900

    err per

--- app/src/main/java/com/example/projectilemotion/MainActivity.kt
+++ app/src/main/java/com/example/projectilemotion/MainActivity.kt
@@ -22,21 +22,28 @@ class MainActivity : AppCompatActivity() {
         error1.visibility= View.INVISIBLE
         val error2 = findViewById<TextView>(R.id.textView7)
         error2.visibility= View.INVISIBLE
-        var err =0
+        var err1 =0
+        var err2 =0
         next.setOnClickListener {
             //Intentオブジェクト生成、遷移画面定義
             if(deg.text.toString().toInt()<0 || deg.text.toString().toInt()>90){
-                err=1
+                err1=1
                 error1.visibility= View.VISIBLE
+            }else{
+                err1=0
             }
 
 
             if(velocity.text.toString().toInt()<0 || velocity.text.toString().toInt()>100 ){
-                err=1
+                err2=1
                 error2.visibility= View.VISIBLE
+            }else{
+                err2=0
             }
 
-            if(err==0) {
+            if(err1==0 && err2==0) {
+                error1.visibility= View.INVISIBLE
+                error2.visibility= View.INVISIBLE
                 val nextIntent = Intent(this, ResultsActivity::class.java)
                 //Intentオブジェクトに入力内容をプットする
                 nextIntent.putExtra("DEGREE", deg.text.toString().toDouble())
