d464f7cba61826394f1d5941c8a5dbd0204e0901
st18180kn <70875769+st18180kn@users.noreply.github.com>
2022-06-17 14:03:39 +0900

    Error1

--- app/src/main/java/com/example/projectilemotion/MainActivity.kt
+++ app/src/main/java/com/example/projectilemotion/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.projectilemotion
 // View Binding
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +18,10 @@ class MainActivity : AppCompatActivity() {
         val velocity = findViewById<EditText>(R.id.velocity)
         val next = findViewById<Button>(R.id.button)
 
+        val error1 = findViewById<TextView>(R.id.textView4)
+        error1.visibility= View.INVISIBLE
+        val error2 = findViewById<TextView>(R.id.textView7)
+        error2.visibility= View.INVISIBLE
          next.setOnClickListener {
              //Intentオブジェクト生成、遷移画面定義
              if(deg.text.toString().toInt()>=0 && deg.text.toString().toInt()<=90){
